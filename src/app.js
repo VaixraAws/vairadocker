9a0ce38429fb2d56228675bf1d1205e90608213c
VaixraAws <vairaaws922000@gmail.com>
2025-05-25 16:37:48 +0530

    third commit

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function app() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '2rem' }}>
+      <h1>Hello from React!</h1>
+      <p>This is a basic deployed React app.</p>
+    </div>
+  );
+}
+
+export default app;
