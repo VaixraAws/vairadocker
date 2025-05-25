b171c0c61e4e0dae912d51bb95a4c930b4a8ee2d
VaixraAws <vairaaws922000@gmail.com>
2025-05-25 17:33:09 +0530

    third commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', paddingTop: '3rem' }}>
+      <h1>Welcome to My Sample React App</h1>
+      <p>This app is running on Ubuntu!</p>
+    </div>
+  );
+}
+
+export default App;
