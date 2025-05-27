312eb137bc69751d82ee6542fc3e6832d91e6ece
VaixraAws <vairaaws922000@gmail.com>
2025-05-27 23:25:52 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div style={{ textAlign: 'center', paddingTop: '3rem' }}>
       <h1>Welcome to My Sample React App</h1>
-      <p>This app is running on Ubuntu!</p>
+      <p>This app is running on Ubuntu Server by Vaira!</p>
     </div>
   );
 }
