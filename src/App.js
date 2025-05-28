8784e3bf6229fd7508baa9dbaa30c1845d3c6124
VaixraAws <vairaaws922000@gmail.com>
2025-05-28 12:51:37 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ function App() {
   return (
     <div style={{ textAlign: 'center', paddingTop: '3rem' }}>
       <h1>Welcome to My Sample React App</h1>
+  <h2>Mamtha loves Vaira</h2>
       <p>This app is running on Ubuntu Server by Vaira!</p>
   <p>Make a change to your repo and git push. Jenkins should auto-trigger the build.</p>
     </div>
